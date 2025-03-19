535ae7d91efc98333adfd72ab5af381d4b72a16e
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-18 23:53:35 -0400

    unit testing for other services template

--- src/test/java/com/example/nobsv2/GetAllProductsServiceTest.java
+++ src/test/java/com/example/nobsv2/GetAllProductsServiceTest.java
@@ -1,6 +1,25 @@
 package com.example.nobsv2;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.nobsv2.product.ProductRepository;
+import com.example.nobsv2.product.services.CreateProductService;
+
 public class GetAllProductsServiceTest {
     // TO IMPLEMENT
+
+    @Mock
+    private ProductRepository productRepository;
+
+    @InjectMocks
+    private CreateProductService createProductService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
 }
