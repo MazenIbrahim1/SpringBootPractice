016962eea7fa32cd8e4bca4ec85c6b503f72edfe
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-20 16:22:31 -0400

    customer controller

--- src/main/java/com/example/nobsv2/mappings/CustomerController.java
+++ src/main/java/com/example/nobsv2/mappings/CustomerController.java
@@ -1,6 +1,10 @@
 package com.example.nobsv2.mappings;
 
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customer")
 public class CustomerController {
 
-    
+    private CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> getCustomerById(@PathVariable Integer id) {
+        Optional<Customer> customer = customerRepository.findById(id);
+        return ResponseEntity.ok().body(customer.get());
+    }
 }
