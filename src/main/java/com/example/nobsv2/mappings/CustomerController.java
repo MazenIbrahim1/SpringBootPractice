ee0ad1a48b761c58f4191813cd96398d12f80cd7
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-20 16:18:32 -0400

    customer repository

--- src/main/java/com/example/nobsv2/mappings/CustomerController.java
+++ src/main/java/com/example/nobsv2/mappings/CustomerController.java
@@ -1,8 +1,12 @@
 package com.example.nobsv2.mappings;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/customer")
 public class CustomerController {
 
+    
 }
