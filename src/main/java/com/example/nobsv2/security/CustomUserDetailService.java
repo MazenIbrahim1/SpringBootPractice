a2a783a45ec6f0026fcd231af74a787a0c2144d2
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-24 22:24:35 -0400

    load user by username

--- src/main/java/com/example/nobsv2/security/CustomUserDetailService.java
+++ src/main/java/com/example/nobsv2/security/CustomUserDetailService.java
@@ -1,8 +1,27 @@
 package com.example.nobsv2.security;
 
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class CustomUserDetailService {
+public class CustomUserDetailService implements UserDetailsService {
 
+    private final CustomUserRepository customUserRepository;
+
+    public CustomUserDetailService(CustomUserRepository customUserRepository) {
+        this.customUserRepository = customUserRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        CustomUser user = customUserRepository.findById(username).get();
+
+        // Add roles and authorities here through relational mappings or other
+
+        return User.withUsername(user.getUsername()).password(user.getPassword()).build();
+    }
+    
 }
