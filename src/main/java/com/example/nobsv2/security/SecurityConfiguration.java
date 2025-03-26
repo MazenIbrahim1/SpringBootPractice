5b305edc33a29233df17c610da02f2e93538da5a
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-25 23:18:19 -0400

    jwt filter

--- src/main/java/com/example/nobsv2/security/SecurityConfiguration.java
+++ src/main/java/com/example/nobsv2/security/SecurityConfiguration.java
@@ -3,21 +3,15 @@ package com.example.nobsv2.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-// import org.springframework.http.HttpMethod;
-// import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-// import org.springframework.security.core.userdetails.User;
-// import org.springframework.security.core.userdetails.UserDetails;
-// import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-// import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import com.example.nobsv2.security.jwt.JwtAuthenticationFilter;
 
 @Configuration
 @EnableMethodSecurity
@@ -65,29 +59,18 @@ public class SecurityConfiguration {
 
                 // Must be at the bottom
                 authorize.anyRequest().authenticated();
-
-
-                // authorize.requestMatchers("/open").permitAll();
-                // authorize.requestMatchers("/closed").authenticated();
-                // // Have to disable CSRF first for PUT, DELETE, and POST
-                // authorize.requestMatchers(HttpMethod.POST, "/product").authenticated();
-
-                // // Adding authority
-                // authorize.requestMatchers(HttpMethod.GET, "/special").hasAuthority("SPECIAL");
-                // authorize.requestMatchers(HttpMethod.GET, "/basic").hasAnyAuthority("SPECIAL", "BASIC");
-
-                // authorize.requestMatchers(HttpMethod.GET, "/special").hasRole("superuser");
                 })
-                // .httpBasic(Customizer.withDefaults())
-                .addFilterBefore(
-                    new BasicAuthenticationFilter(authenticationManager(httpSecurity)),
-                    UsernamePasswordAuthenticationFilter.class
-                )
+                .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
                 .build();
         } catch (Exception e) {
             System.out.println(e);
         }
 
         return null;
+    }
+    
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter() {
+        return new JwtAuthenticationFilter();
     }
 }
