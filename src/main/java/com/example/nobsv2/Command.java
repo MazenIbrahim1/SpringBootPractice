1865ee9b16989f7353f3961363bba39777070017
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-28 23:36:18 -0400

    info

--- src/main/java/com/example/nobsv2/Command.java
+++ src/main/java/com/example/nobsv2/Command.java
@@ -2,6 +2,7 @@ package com.example.nobsv2;
 
 import org.springframework.http.ResponseEntity;
 
+// For services that require body such as POST
 public interface Command<I, O> {
     ResponseEntity<O> execute(I input);
 }
--- src/main/java/com/example/nobsv2/Query.java
+++ src/main/java/com/example/nobsv2/Query.java
@@ -2,6 +2,7 @@ package com.example.nobsv2;
 
 import org.springframework.http.ResponseEntity;
 
+// For GET services
 public interface Query<I, O> {
     ResponseEntity<O> execute(I input);
 }
