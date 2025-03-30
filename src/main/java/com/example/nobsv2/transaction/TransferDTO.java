e01836ebb9b87ef65b1c29fce815d0ad0e5a9cb8
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-03-29 22:05:09 -0400

    constructor

--- src/main/java/com/example/nobsv2/transaction/TransferDTO.java
+++ src/main/java/com/example/nobsv2/transaction/TransferDTO.java
@@ -1,8 +1,12 @@
 package com.example.nobsv2.transaction;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class TransferDTO {
     private String fromUser;
     private String toUser;
