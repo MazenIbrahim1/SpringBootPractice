442e499855bcc7996c7fcc5c282a9db5fe5bf07c
MazenIbrahim1 <mazen.ibrahim@stonybrook.edu>
2025-04-02 20:21:28 -0400

    cat fact test

--- src/test/java/com/example/nobsv2/CatFactAPITest.java
+++ src/test/java/com/example/nobsv2/CatFactAPITest.java
@@ -1,6 +1,25 @@
 package com.example.nobsv2;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.example.nobsv2.catfact.CatFactController;
+import com.example.nobsv2.catfact.CatFactService;
+
 public class CatFactAPITest {
-    // To implement
+    // To Implement
+
+    @Mock
+    private CatFactController catFactController;
+
+    @InjectMocks
+    private CatFactService catFactService;
+
+    @BeforeEach
+    public void setup() {
+        MockitoAnnotations.openMocks(this);
+    }
 
 }
